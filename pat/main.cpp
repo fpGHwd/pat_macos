4858d7125134dc6e6098b2d6ae9afcbff519a423
wdjxysc <wdjxysc@163.com>
2018-02-21 18:27:40 +0800

    Initial Commit

---
+++ pat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pat
+//
+//  Created by 帝君 on 2018/2/21.
+//  Copyright © 2018年 Rumi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
